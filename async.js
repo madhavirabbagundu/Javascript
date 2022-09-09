0f8076b88abc3742d6a4555bc09644a7f6300e17
madhavirabbagungu@gmail.com <madhavirabbgundu@gmail.com>
2022-09-10 00:16:06 +0530

    async

---
+++ async.js
@@ -0,0 +1,8 @@
+// asynchrounous functions are very important because, 
+// suppose, we have lot of function is there, in that time it does wait for the another function
+// it takes lot of time to implement the things. so, 
+// the async funiton are the main things it will help for the more
+// because, when we use the async function it will print the first main things then the another function 
+// when we use the settimeout then async function is excuted
+// it is very useful for us
+// 
---
+++ hoisting.js
@@ -0,0 +1,7 @@
+// hoisting is the javascript process because, hoisting is the excuted before the code excution
+// hoisting is the moving the process.
+// in the hoisting var variable is working but le and const are not work because, 
+// let and const are not intialized before the code write
+
+// 
+// 
