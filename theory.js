80ed95ed49548ead0aad211c3eea68ec25deadf3
Madhavi Rabbagundu <madhavirabbagundu@gmail.com>
2023-04-24 21:15:26 +0530

    theory questions

---
+++ theory.js
@@ -0,0 +1,8 @@
+//closures give you access to an outer function scope from an inner function
+
+//callback function is afunction that pass arguments to an another fnction then that function is excuted another function is called the callback function. 
+// callback function is waitt for tehe another function.
+
+//hoisting in javascipt is a function when a function or variable are excuted before the function excution.
+
+//promises is a object . it will handle the asynchronous operations .
